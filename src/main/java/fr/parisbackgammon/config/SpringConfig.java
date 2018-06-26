8e57a4ec4e08a330cfac88aa927d77c31888084e
Master-Goldeau <34507362+Master-Goldeau@users.noreply.github.com>
2018-06-26 22:32:18 +0200

    Add files via upload

---
+++ src/main/java/fr/parisbackgammon/config/SpringConfig.java
@@ -0,0 +1,106 @@
+package fr.parisbackgammon.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+/**
+ * Configuration générale de l'injection de dépendances via Spring.
+ * 
+ * @author Etienne Savary
+ *
+ */
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableJpaRepositories(basePackages = { "fr.parisbackgammon" })
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = { "fr.parisbackgammon" })
+public class SpringConfig {
+	
+	/**
+	 * Variable definissant l'environemment de travail (driver BDD)
+	 */
+	@Autowired
+	private Environment environment;
+
+	/**
+	 * @return data source (BDD, driver, url et accès,)
+	 */
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getProperty("jdbc.url"));
+		dataSource.setUsername(environment.getProperty("jdbc.username"));
+		dataSource.setPassword(environment.getProperty("jdbc.password"));
+		return dataSource;
+	}
+	
+	/**
+	 * Mise en place du management des transactions avec la BDD
+	 * @param entityManagerFactory : 
+	 * @return Le manager des transactions avec le BDD
+	 */
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+		jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+		// Exécution du database populator (optionnel)
+		// DatabasePopulatorUtils.execute(databasePopulator(), dataSource());
+		return jpaTransactionManager;
+	}
+	
+	/**
+	 * Choix du type de data base pour JPA (ici MySQL)
+	 * @return
+	 */
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		jpaVendorAdapter.setDatabase(Database.MYSQL);
+		jpaVendorAdapter.setShowSql(true);
+		return jpaVendorAdapter;
+	}
+
+	/**
+	 * Mise en place de la communication avec la base de données via un container
+	 * (Emplacement des JavaBean à sauvegarder et définition du lancement de la database)
+	 * @return
+	 */
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource());
+		entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter());
+		entityManagerFactory.setPackagesToScan("fr.proxibanque.proxibanquesi.model");
+		Properties jpaProperties = new Properties();
+		jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		entityManagerFactory.setJpaProperties(jpaProperties);
+		return entityManagerFactory;
+	}
+	
+
+}
